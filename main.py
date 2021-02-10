d3af4789891bb699a41b081a4f509eea93d5a70a
Douglass Murray <douglass.murray@gmail.com>
2021-02-10 10:27:03 -0500

    More PEP8 convensions on main.py script.

--- main.py
+++ main.py
@@ -11,11 +11,12 @@ from opampnoiseanalysis.inverting import *
 from opampnoiseanalysis.plotter import *
 
 def interface():
-    """General interface function for choosing which op-amp/topology to calculate noise."""
+    """General interface function for choosing which op-amp/topology to
+        calculate noise."""
     while True:
         try:
-            choice = int(input("Noise of (1) op-amp, (2) inverting topology, "\
-                "or (3) non-inverting topology: "))
+            choice = int(input("Noise of (1) op-amp, (2) inverting topology, "
+                               "or (3) non-inverting topology: "))
         except ValueError:
             print("Please choose options 1, 2, or 3.")
             continue
@@ -24,8 +25,11 @@ def interface():
                 vnoise_low_hz, vnoise_high_hz, inoise_low_hz, inoise_high_hz,
                 inoise_at_hz, amp_gain_bandwidth = opamp_choose_input()
                 freq, vnoise, inoise = opamp_noise(vnoise_low_hz,
-                    vnoise_high_hz, inoise_low_hz, inoise_high_hz,
-                    inoise_at_hz, amp_gain_bandwidth)
+                                                   vnoise_high_hz,
+                                                   inoise_low_hz,
+                                                   inoise_high_hz,
+                                                   inoise_at_hz,
+                                                   amp_gain_bandwidth)
                 print(freq, vnoise, inoise)
                 generic_opamp_noise_plot(freq, vnoise, inoise)
             elif choice == 2:
@@ -36,12 +40,12 @@ def interface():
                 r_one = float(input("Input R1 (Ohm): "))
                 r_two = float(input("Input R2 (Ohm): "))
                 r_three = float(input("Input R3 (Ohm): "))
-                at_freq = float(input("Input reference freq (Hz), "\
-                    "default 1000: "))
-                low_freq_of_interest = float(input("Input lower freq of "\
-                    "interest (Hz): "))
-                high_freq_of_interest = float(input("Input upper freq of "\
-                    "interest (Hz): "))
+                at_freq = float(input("Input reference freq (Hz), "
+                                      "default 1000: "))
+                low_freq_of_interest = float(input("Input lower freq of "
+                                                   "interest (Hz): "))
+                high_freq_of_interest = float(input("Input upper freq of "
+                                                    "interest (Hz): "))
 
                 # Op-amp specific parameters based on datasheet
                 vnoise_low_hz, vnoise_high_hz, inoise_low_hz, inoise_high_hz,
@@ -56,11 +60,15 @@ def interface():
                     temp)
                 print("Max Noise BW:", max_noise_bandwidth, " Hz")
                 print("Noise over bandwidth: ", integrated_noise, " Vrms")
-                
+
                 # Totatl RTI noise
                 rti_noise = inverting_rti_noise(r_source, r_one, r_two,
-                    r_three, vnoise_low_hz, vnoise_high_hz, inoise_low_hz,
-                    inoise_high_hz, at_freq, inoise_at_hz, temp)
+                                                r_three, vnoise_low_hz,
+                                                vnoise_high_hz,
+                                                inoise_low_hz,
+                                                inoise_high_hz,
+                                                at_freq, inoise_at_hz,
+                                                temp)
                 print("RTI Noise: ", rti_noise, " V/sqrt(Hz)")
             elif choice == 3:
                 noninverting_topo_image_display()
@@ -77,5 +85,5 @@ if __name__ == '__main__':
     print(r"/ /_/ / /_/ /  / ___ |/ / / / / / /_/ /  / /|  / /_/ / (__  )  __/  / /___/ /_/ / / /__  ")
     print(r"\____/ .___/  /_/  |_/_/ /_/ /_/ .___/  /_/ |_/\____/_/____/\___/   \____/\__,_/_/\___/  ")
     print(r"    /_/                       /_/                                                       " )
-    
+
     interface()

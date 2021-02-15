c20d7aee94f833064888fce1f0f7f3d1421a31f3
Doug Murray <murray@twinleaf.com>
2021-02-15 16:08:11 -0500

    Changed default values of mt048 script.  Note that there needs some if statements as it gets NaNs if noise_corner_freq is lower than low_freq_interest.

--- opampnoiseanalysis/mt048.py
+++ opampnoiseanalysis/mt048.py
@@ -3,10 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 freqs = np.arange(0.1, 1e6)
-noise_corner_freq = 15
+noise_corner_freq = 0.7
 opamp_noise = 0.9e-9
-high_freq_interest = 700000
-low_freq_interest = 1000
+high_freq_interest = 10
+low_freq_interest = 0.1
 
 # op-amp spectral noise density (V/sqrt(Hz))
 pink_freq_range = freqs[freqs < noise_corner_freq]

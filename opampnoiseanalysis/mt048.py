02559b301edb0a88acfe0e100bae4740ff95afb0
Doug Murray <murray@twinleaf.com>
2021-02-15 15:24:26 -0500

    Added some print statements to mt048 script.

--- opampnoiseanalysis/mt048.py
+++ opampnoiseanalysis/mt048.py
@@ -14,15 +14,22 @@ white_freq_range = freqs[freqs > noise_corner_freq]
 pink_noise_spectral_density = opamp_noise * np.sqrt(noise_corner_freq) * np.sqrt(1 / pink_freq_range)
 white_noise_spectral_density = np.full_like(white_freq_range, opamp_noise)
 opamp_total_noise_spectral_density = np.concatenate((pink_noise_spectral_density, white_noise_spectral_density), axis=None)  # V/sqrt(Hz)
+print("Total Spectral Noise Density (V/sqrt(Hz)): ", opamp_total_noise_spectral_density)
 
 # RMS noise (Vrms)
 rms_noise_low_freq = opamp_noise * np.sqrt(noise_corner_freq * np.log(noise_corner_freq / low_freq_interest))
 rms_noise_high_freq = opamp_noise * np.sqrt(high_freq_interest - noise_corner_freq)
 # rms_total_noise = opamp_noise * np.sqrt((noise_corner_freq * np.log(noise_corner_freq / low_freq_interest)) + (high_freq_interest - noise_corner_freq))
 rms_total_noise = np.sqrt(np.square(rms_noise_low_freq) + np.square(rms_noise_high_freq))
+print("From %f to %f Hz bandwidth" % (low_freq_interest, high_freq_interest))
+print("Total RMS Noise (Vrms): ", rms_total_noise)
 
 # Peak-to-peak noise (V)
 noise_p_p = 6.6 * rms_total_noise
+print("Noise peak-to-peak (V_p_p): ", noise_p_p)
 
 plt.loglog(freqs, opamp_total_noise_spectral_density)
+plt.title("Total Noise Spectral Density")
+plt.xlabel("Frequnecy (Hz)")
+plt.ylabel("Noise (V/sqrt(Hz))")
 plt.show()
